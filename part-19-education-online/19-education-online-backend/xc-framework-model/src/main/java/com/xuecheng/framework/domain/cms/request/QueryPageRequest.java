e7d857d7b379fa3bbd4d046bbc5b1d0c7969ac4c
phoenix fate <sm516116978@outlook.com>
2022-03-18 01:12:44 +0800

    学成教育day01

---
+++ part-19-education-online/19-education-online-backend/xc-framework-model/src/main/java/com/xuecheng/framework/domain/cms/request/QueryPageRequest.java
@@ -0,0 +1,33 @@
+package com.xuecheng.framework.domain.cms.request;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @author Administrator
+ * @version 1.0
+ * @create 2018-09-12 14:59
+ **/
+@Data
+@ApiModel("页面查询的查询条件")
+public class QueryPageRequest {
+    //接收页面查询的查询条件
+    //站点id
+    //站点id
+    @ApiModelProperty("站点id")
+    private String siteId;
+    //页面id
+    @ApiModelProperty("页面id")
+    private String pageId;
+    //页面名称
+    @ApiModelProperty("页面名称")
+    private String pageName;
+    //别名
+    @ApiModelProperty("别名")
+    private String pageAliase;
+    //模版id
+    @ApiModelProperty("模版id")
+    private String templateId;
+    //....
+}
