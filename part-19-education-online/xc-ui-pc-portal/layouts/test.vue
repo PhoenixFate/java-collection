0f4a45a44c7306171f9291d7a4ab6faa7b41404d
phoenix fate <sm516116978@outlook.com>
2022-05-13 00:40:13 +0800

    nuxt

---
+++ part-19-education-online/xc-ui-pc-portal/layouts/test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    这里是头部区域
+    <nuxt/>
+    这里是尾部区域
+  </div>
+</template>
+<script>
+  export default {
+    components: {
+    }
+  }
+</script>
+<style>
+
+</style>
---
+++ part-19-education-online/xc-ui-pc-portal/pages/user.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    用户管理导航页面 <nuxt-link :to="'/user/one'">进入到one页面</nuxt-link>
+    <nuxt-child/>
+  </div>
+</template>
+
+<script>
+export default {
+  layout:'test'
+}
+</script>
+
+<style scoped>
+
+</style>
