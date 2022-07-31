7c4b62744f511941f6eed73c9a9c0fffe6e0ca5d
phoenixfate <sm516116978@outlook.com>
2022-08-01 01:16:04 +0800

    activiti7 service：流程审批历史；待办任务

--- activiti7/activiti7-service/src/main/java/com/phoenix/workflow/service/impl/BusinessStatusService.java
+++ activiti7/activiti7-service/src/main/java/com/phoenix/workflow/service/impl/BusinessStatusService.java
@@ -5,7 +5,10 @@ import com.phoenix.workflow.entity.BusinessStatus;
 import com.phoenix.workflow.enums.BusinessStatusEnum;
 import com.phoenix.workflow.mapper.BusinessStatusMapper;
 import com.phoenix.workflow.service.IBusinessStatusService;
+import com.phoenix.workflow.utils.Result;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class BusinessStatusService extends ServiceImpl<BusinessStatusMapper, BusinessStatus> implements IBusinessStatusService {
@@ -16,5 +19,25 @@ public class BusinessStatusService extends ServiceImpl<BusinessStatusMapper, Bus
         businessStatus.setStatus(BusinessStatusEnum.WAIT.getCode());
         businessStatus.setBusinessKey(businessKey);
         return baseMapper.insert(businessStatus);
+    }
+
+    @Override
+    public Result updateState(String businessKey, BusinessStatusEnum statusEnum, String processInstanceId) {
+        //1.查询当前数据
+        BusinessStatus businessStatus = baseMapper.selectById(businessKey);
+        //2.设置状态值
+        businessStatus.setStatus(statusEnum.getCode());
+        businessStatus.setUpdateDate(new Date());
+        if(processInstanceId!=null){
+            businessStatus.setProcessInstanceId(processInstanceId);
+        }
+        //3.更新操作
+        baseMapper.updateById(businessStatus);
+        return Result.ok();
+    }
+
+    @Override
+    public Result updateState(String businessKey, BusinessStatusEnum statusEnum) {
+        return updateState(businessKey,statusEnum,null);
     }
 }
