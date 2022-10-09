2216ce8b365fa6e29c97004c7e1f921df057d001
phoenixfate <sm516116978@outlook.com>
2022-10-10 01:40:50 +0800

    博客系统 项目搭建

---
+++ dream-blog/dream-blog-backend-parent/dream-blog-api/src/main/java/com/phoenix/blog/entity/Category.java
@@ -0,0 +1,52 @@
+package com.phoenix.blog.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@TableName("mxg_category") // Category实体类对应表mxg_category
+@Data
+public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 主键，分布式id
+     * IdType.ASSIGN_ID: 分配ID (主键类型为number或string）, 默认实现类 com.baomidou.mybatisplus.core.incrementer.DefaultIdentifierGenerator(雪花算法)
+     */
+    @TableId(value = "id", type= IdType.ASSIGN_ID)
+    private String id;
+
+    /**
+     * 分类名称
+     */
+    private String name;
+
+    /**
+     * 备注
+     */
+    private String remark;
+
+    /**
+     * 状态(1:正常，0:禁用)
+     */
+    private Integer status;
+
+    /**
+     * 排序
+     */
+    private Integer sort;
+
+    /**
+     * 创建时间
+     */
+    private Date createDate;
+
+    /**
+     * 更新时间
+     */
+    private Date updateDate;
+}
--- dream-blog/dream-blog-backend-parent/dream-blog-common/src/main/java/com/phoenix/blog/common/base/Result.java
+++ dream-blog/dream-blog-backend-parent/dream-blog-common/src/main/java/com/phoenix/blog/common/base/Result.java
@@ -5,8 +5,7 @@ import com.phoenix.blog.common.constant.ResultEnum;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.Serializable;
 
@@ -16,9 +15,8 @@ import java.io.Serializable;
 @Data
 @NoArgsConstructor // 无参构造方法
 @AllArgsConstructor // 有参构造方法
+@Slf4j
 public final class Result implements Serializable {
-
-    private static final Logger logger = LoggerFactory.getLogger(Result.class);
 
     private static final long serialVersionUID = 1L;
 
@@ -50,21 +48,21 @@ public final class Result implements Serializable {
     }
 
     public static Result error(String message) {
-        logger.debug("返回错误：code={}, message={}", ResultEnum.ERROR.getCode(), message);
+        log.debug("返回错误：code={}, message={}", ResultEnum.ERROR.getCode(), message);
         return new Result(ResultEnum.ERROR.getCode(), message, null);
     }
 
     public static Result build(int code, String message) {
-        logger.debug("返回结果：code={}, message={}", code, message);
+        log.debug("返回结果：code={}, message={}", code, message);
         return new Result(code, message, null);
     }
 
     public static Result build(ResultEnum resultEnum) {
-        logger.debug("返回结果：code={}, message={}", resultEnum.getCode(), resultEnum.getDesc());
+        log.debug("返回结果：code={}, message={}", resultEnum.getCode(), resultEnum.getDesc());
         return new Result(resultEnum.getCode(), resultEnum.getDesc(), null);
     }
-	
-	public String toJsonString() {
+
+    public String toJsonString() {
         return JSON.toJSONString(this);
     }
 
