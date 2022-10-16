a36bbe43f4875db9608bdebdaf22774dc3f9a0c1
phoenixfate <sm516116978@outlook.com>
2022-10-17 01:39:14 +0800

    递归删除菜单

--- spring-security/mine-spring-security-parent/mine-spring-security-web/src/main/java/com/phoenix/web/service/impl/SysPermissionServiceImpl.java
+++ spring-security/mine-spring-security-parent/mine-spring-security-web/src/main/java/com/phoenix/web/service/impl/SysPermissionServiceImpl.java
@@ -1,17 +1,14 @@
 package com.phoenix.web.service.impl;
 
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.phoenix.web.entity.SysPermission;
 import com.phoenix.web.mapper.SysPermissionMapper;
 import com.phoenix.web.service.SysPermissionService;
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.security.Permission;
 import java.util.ArrayList;
 import java.util.List;
 
