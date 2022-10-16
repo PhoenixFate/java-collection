804392d051405e6b81ba4ed4a625d62aec586d9f
phoenixfate <sm516116978@outlook.com>
2022-10-17 01:38:52 +0800

    递归删除菜单

--- spring-security/mine-spring-security-parent/mine-spring-security-web/src/main/java/com/phoenix/web/service/impl/SysPermissionServiceImpl.java
+++ spring-security/mine-spring-security-parent/mine-spring-security-web/src/main/java/com/phoenix/web/service/impl/SysPermissionServiceImpl.java
@@ -1,13 +1,18 @@
 package com.phoenix.web.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.phoenix.web.entity.SysPermission;
 import com.phoenix.web.mapper.SysPermissionMapper;
 import com.phoenix.web.service.SysPermissionService;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.security.Permission;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -29,11 +34,38 @@ public class SysPermissionServiceImpl extends ServiceImpl<SysPermissionMapper, S
     @Transactional
     @Override
     public boolean deleteById(Long id) {
-        //1.删除当前id
-        baseMapper.deleteById(id);
-        //2.删除parent_id=id
-        //delete from sys_permission where  parent_id = #{id}
-        baseMapper.delete(new LambdaQueryWrapper<SysPermission>().eq(SysPermission::getParentId, id));
+        // 1.删除当前id
+        // baseMapper.deleteById(id);
+        // 2.删除parent_id=id
+        // delete from sys_permission where  parent_id = #{id}
+        // baseMapper.delete(new LambdaQueryWrapper<SysPermission>().eq(SysPermission::getParentId, id));
+
+        if (id == null) {
+            return false;
+        }
+        //要删除的所有评论id（通过递归获得）
+        List<Long> idListToDelete = new ArrayList<>();
+        idListToDelete.add(id);
+        //递归所有的评论id，并将id装到要删除的集合中
+        this.getAllIdListByParentId(idListToDelete, id);
+        //批量删除集合中的评论id
+        baseMapper.deleteBatchIds(idListToDelete);
         return true;
+    }
+
+    private void getAllIdListByParentId(List<Long> idListToDelete, Long parentId) {
+        //查询子评论信息
+        QueryWrapper<SysPermission> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("parent_id", parentId);
+        List<SysPermission> permissionList = baseMapper.selectList(queryWrapper);
+        //如果子评论不为空，则取出每条评论的评论id
+        if (CollectionUtils.isNotEmpty(permissionList)) {
+            for (SysPermission permission : permissionList) {
+                //将当前查询到的评论id放到要删除的id集合中
+                idListToDelete.add(permission.getId());
+                //递归继续查询子评论id
+                this.getAllIdListByParentId(idListToDelete, permission.getId());
+            }
+        }
     }
 }
