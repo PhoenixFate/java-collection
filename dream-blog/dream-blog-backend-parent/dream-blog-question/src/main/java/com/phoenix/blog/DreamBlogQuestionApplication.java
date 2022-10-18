7e3becfc2c5c21d17b1efbd69a96943b384753b7
phoenixfate <sm516116978@outlook.com>
2022-10-18 18:08:45 +0800

    博客系统--对接nacos

--- dream-blog/dream-blog-backend-parent/dream-blog-question/src/main/java/com/phoenix/blog/DreamBlogQuestionApplication.java
+++ dream-blog/dream-blog-backend-parent/dream-blog-question/src/main/java/com/phoenix/blog/DreamBlogQuestionApplication.java
@@ -3,7 +3,14 @@ package com.phoenix.blog;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * @Author phoenix
+ * @Date 2022/10/10 10:45
+ * @Version 1.0.0
+ */
+@EnableDiscoveryClient //spring cloud原生注解 开启nacos服务注册与发现
 @EnableSwagger2Doc // 开启swagger功能
 @SpringBootApplication
 public class DreamBlogQuestionApplication {
