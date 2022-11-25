d13af5f42413d2ccdb1f57494cab78b00883ccba
phoenixfate <sm516116978@outlook.com>
2022-11-25 18:10:04 +0800

    lambda

---
+++ base-test/base-lambda/src/main/java/com/phoenix/Student.java
@@ -0,0 +1,40 @@
+package com.phoenix;
+
+/**
+ * @Author phoenix
+ * @Date 2022/11/25 10:11
+ * @Version 1.0.0
+ */
+public class Student {
+    private int id;
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ base-test/base-lambda/src/main/java/com/phoenix/TestLambda01.java
@@ -0,0 +1,47 @@
+package com.phoenix;
+
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.TreeSet;
+import java.util.stream.Stream;
+
+/**
+ * @Author phoenix
+ * @Date 2022/11/25 9:55
+ * @Version 1.0.0
+ */
+public class TestLambda01 {
+
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("hello");
+            }
+        }).start();
+        new Thread(() -> {
+            System.out.println("hello2");
+        }).start();
+
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("AAAAA");
+        list.add("BBBBB");
+        list.add("CCCCC");
+        list.add("DDDDD");
+        //形参的类型是确定的，可省略；只有一个形参，()可以省略；
+        list.forEach(t -> System.out.print(t + "\t"));
+        //或者更简洁的方法引用：list.forEach(System.out::println);
+        //Random random = new Random();
+        //Stream<Integer> stream = Stream.generate(() -> random.nextInt(100));
+        //stream.forEach(t -> System.out.println(t));
+
+        Collator collator = Collator.getInstance();
+        TreeSet<Student> set = new TreeSet<>((s1, s2) -> collator.compare(s1.getName(), s2.getName()));
+        set.add(new Student(10, "张飞"));
+        set.add(new Student(3, "周瑜"));
+        set.add(new Student(1, "宋江"));
+        set.forEach(student -> System.out.println(student));
+    }
+}
