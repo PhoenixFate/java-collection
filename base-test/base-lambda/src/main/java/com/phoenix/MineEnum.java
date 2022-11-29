39126a07938a667208f85648888850a9b5aa7834
phoenixfate <sm516116978@outlook.com>
2022-11-30 01:39:52 +0800

    test

---
+++ base-test/base-lambda/src/main/java/com/phoenix/MineEnum.java
@@ -0,0 +1,14 @@
+package com.phoenix;
+
+/**
+ * @Author phoenix
+ * @Date 11/30/22 00:15
+ * @Version 1.0
+ */
+public enum MineEnum {
+
+    ONE_ENUM,
+
+    TWO_ENUM;
+
+}
---
+++ base-test/base-lambda/src/main/java/com/phoenix/TestEnum.java
@@ -0,0 +1,16 @@
+package com.phoenix;
+
+/**
+ * @Author phoenix
+ * @Date 11/30/22 00:15
+ * @Version 1.0
+ */
+public class TestEnum {
+
+    public static void main(String[] args) {
+        System.out.println(MineEnum.ONE_ENUM);
+        System.out.println(MineEnum.TWO_ENUM);
+
+    }
+
+}
